8690ada14ec663ce778164b6af0c0c52853a2af3
lexach91 <ronmctailor@protonmail.com>
2021-12-10 10:10:24 +0800

    Add code to generate grid, tetrominoes variables, and basic style

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+function getElements(className) {
+    return document.getElementsByClassName(className);
+}
+
+function updateHtml(id, html) {
+    getElement(id).innerHTML = html;
+}
+
+const grid = getElement('grid');
+// create grid for tetris
+for (let i = 0; i < 20; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    for (let j = 0; j < 10; j++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        row.appendChild(cell);
+    }
+    grid.appendChild(row);
+}
+
+// create tetris pieces
+const iPiece = [
+    [1, 1, 1, 1]
+];
+const oPiece = [
+    [1, 1],
+    [1, 1]
+];
+const tPiece = [
+    [1, 1, 1],
+    [0, 1, 0]
+];
+const lPiece = [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+];
+const jPiece = [
+    [0, 1],
+    [0, 1],
+    [1, 1]
+];
+const zPiece = [
+    [1, 1, 0],
+    [0, 1, 1]
+];
+const sPiece = [
+    [0, 1, 1],
+    [1, 1, 0]
+];
