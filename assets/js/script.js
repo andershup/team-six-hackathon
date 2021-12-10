f2edd9173e8724f2a5f5ddcf1cef21e16672fe2f
lexach91 <ronmctailor@protonmail.com>
2021-12-10 14:45:37 +0800

    Add functions to draw and remove tetris pieces

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,49 +11,140 @@ function updateHtml(id, html) {
 }
 
 const grid = getElement('grid');
+
 // create grid for tetris
 for (let i = 0; i < 20; i++) {
     const row = document.createElement('div');
     row.className = 'row';
+    row.id = `row ${i}`;
     for (let j = 0; j < 10; j++) {
         const cell = document.createElement('div');
         cell.className = 'cell';
+        cell.id = `cell ${i} ${j}`;
         row.appendChild(cell);
     }
     grid.appendChild(row);
 }
 
+
 // create tetris pieces
-const iPiece = [
+const iShape = [
     [1, 1, 1, 1]
 ];
-const oPiece = [
+const oShape = [
     [1, 1],
     [1, 1]
 ];
-const tPiece = [
+const tShape = [
     [1, 1, 1],
     [0, 1, 0]
 ];
-const lPiece = [
+const lShape = [
     [1, 0],
     [1, 0],
     [1, 1]
 ];
-const jPiece = [
+const jShape = [
     [0, 1],
     [0, 1],
     [1, 1]
 ];
-const zPiece = [
+const zShape = [
     [1, 1, 0],
     [0, 1, 1]
 ];
-const sPiece = [
+const sShape = [
     [0, 1, 1],
     [1, 1, 0]
 ];
-const pieces = [iPiece, oPiece, tPiece, lPiece, jPiece, zPiece, sPiece];
+const shapes = [iShape, oShape, tShape, lShape, jShape, zShape, sShape];
+
+// function rotatePiece(piece) {
+//     const newPiece = [];
+//     for (let i = 0; i < piece[0].length; i++) {
+//         const row = [];
+//         for (let j = 0; j < piece.length; j++) {
+//             row.push(piece[j][i]);
+//         }
+//         newPiece.push(row);
+//     }
+//     return newPiece;
+// }
+
+function getRandomShape() {
+    return shapes[Math.floor(Math.random() * shapes.length)];
+}
+
+
+function getNextPiece() {
+    return {
+        shape: getRandomShape(),
+        row: 0,
+        col: 3
+    }
+}
+
+let currentPiece = getNextPiece();
+
+function drawPiece(piece) {
+    row = piece.row;
+    col = piece.col;
+    shape = piece.shape;
+    for (let i = 0; i < shape.length; i++) {
+        for (let j = 0; j < shape[i].length; j++) {
+            if (shape[i][j] === 1) {
+                getElement(`cell ${row + i} ${col + j}`).classList.add('active-piece');
+            }
+        }
+    }
+}
+
+function undrawPiece(piece) {
+    row = piece.row;
+    col = piece.col;
+    shape = piece.shape;
+    for (let i = 0; i < shape.length; i++) {
+        for (let j = 0; j < shape[i].length; j++) {
+            if (shape[i][j] === 1) {
+                getElement(`cell ${row + i} ${col + j}`).classList.remove('active-piece');
+            }
+        }
+    }
+}
+
+
+drawPiece(currentPiece);
+
+// function placePiece(piece, row, col) {
+//     for (let i = 0; i < piece.length; i++) {
+//         for (let j = 0; j < piece[i].length; j++) {
+//             if (piece[i][j] === 1) {
+//                 getElement(`cell ${row + i} ${col + j}`).classList.add('active-piece');
+//             }
+//         }
+//     }
+// }
+
+function removePiece(piece) {
+    
+}
+
+
+// function movePieceLeft() {
+
+// }
+
+// let currentPiece = getRandomPiece();
+// let anotherPiece = getRandomPiece();
+
+// placePiece(currentPiece, 0, 0);
+// placePiece(anotherPiece, 0, 4);
+
+
+
+
+
+
 
 // Buttons 
 const startButton = getElement('start');
@@ -65,5 +156,16 @@ let gameOver = false;
 let gamePaused = false;
 let gameStarted = false;
 let gameInterval;
-let currentPiece;
+// let currentPiece;
 // TETRIS
+
+startButton.addEventListener('click', () => {
+    if (!gameStarted) {
+        gameStarted = true;
+        console.log('game started', gameStarted);
+    } 
+});
+
+while (!gameOver && gameStarted) {
+
+}
